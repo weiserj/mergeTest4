e66e2c0505993e672a72ffe36864e3ae471cf152
Johann Weiser <johann.weiser@aon.at>
2017-03-14 11:00:08 +0100

    1. Version

---
+++ src/weiser/Hallo4.java
@@ -0,0 +1,18 @@
+package weiser;
+
+public class Hallo4 {
+
+	public static void gutenTag() {
+		System.out.println("Guten Tag");
+	}
+
+	public static void byby() {
+		System.out.println("Auf Wiedersehen.");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
